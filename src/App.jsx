08091395b25a2f40b803041e10fa944c67252d94
Lex <info@alexandranel.com>
2024-05-23 10:50:05 +1000

    updated footer links ofr public

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ export default function App() {
   const logoImage = <img src={logo} alt="Company Logo" style={{ width: '100px' }} />; //defining logo in app for use across children
   const contactInfo = <p><a href="mailto:info@alexandranel.com">Contact</a></p>;
   const git = <p><a href="https://github.com/AlexandraNel">GitHub</a></p>;
-  const linkedIn= <a href="https://www.linkedin.com/in/alexa-nel/"><img src= '/public/In-Blue.png'></img></a>;
-  const instagram= <a href="https://www.instagram.com/alexa_n_motion/"><img src= '/public/instagram.svg'></img></a>;
+  const linkedIn= <a href="https://www.linkedin.com/in/alexa-nel/"><img src= '/In-Blue.png'></img></a>;
+  const instagram= <a href="https://www.instagram.com/alexa_n_motion/"><img src= '/instagram.svg'></img></a>;
 
   
   return (
